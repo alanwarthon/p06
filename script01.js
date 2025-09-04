ad1835a7d681d1879ccf6c3bd6e988c769a509ff
alanwarthon <familiawarthon@gmail.com>
2025-09-03 19:18:02 -0700

    Creating an object

--- script01.js
+++ script01.js
@@ -1,30 +1,32 @@
 const btn01 = document.getElementById("btnProcess");
-btn01.addEventListener("click",process02);
+btn01.addEventListener("click",printData);
 
 const msg01 = document.getElementById("txt01");
 const msg02 = document.getElementById("txt02");
+const msg03 = document.getElementById("txt03");
+const msg04 = document.getElementById("txt04");
 const txtMessage = document.getElementById("txtS");
 
+
+
+
+
 // Usa Number() si quieres asegurarte de que el valor sea solo un número válido.
-function process01() {
+function printData() {
+
+  // creating the object
+  const costumer = {
+  name: msg01.value,
+  age: msg02.value,
+  city: msg03.value,
+  gpa: msg04.value
+  };
+
+  let finalMessage = "********************* REPORT *********************\n";
+  txtMessage.textContent = finalMessage;
+  console.log(msg01.value);
+  console.log(typeof(msg01.value));
+  txtMessage.textContent += `Name: ${costumer.name}\nAge: ${costumer.age}\nCity: ${costumer.city}\nGPA: ${costumer.gpa} `;
   
-  let sum = Number(msg01.value) + Number(msg02.value);
-  let finalMessage = `${msg01.value} + ${msg02.value} = ${sum}`;
 
-  txtMessage.textContent += finalMessage + "\n";
-  msg01.value = "";
-  msg02.value = "";
-  msg01.focus();
-
-}
-
-// Usa parseFloat() si quieres ser más permisivo y permitir entradas con texto mezclado.
-function process02(){
-  let sum = parseFloat(msg01.value) + parseFloat(msg02.value);
-  let finalMessage = `${msg01.value} + ${msg02.value} = ${sum}`;
-
-  txtMessage.textContent += finalMessage + "\n";
-  msg01.value = "";
-  msg02.value = "";
-  msg01.focus();
 }
